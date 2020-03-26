3c8c7b1014821acd63035c96d69e89a8e5ec2959
GalAzaria <44269503+GalAzaria@users.noreply.github.com>
2020-03-26 20:28:02 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
     $('.menu').on('click', function () {
         $(this).toggleClass('open');
-        $('.top-nav').toggleClass('open')
-    })
+        $('.top-nav').toggleClass('open');
+
+    });
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu').removeClass('open');
+        $('.top-nav').removeClass('open');
+
+    });
 });
